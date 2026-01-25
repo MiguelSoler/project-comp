3e120fdbd30e758d7dc1cc8ff439d89655d26b7c
MiguelSoler <miguelgarciasoler1@hotmail.com>
2026-01-25 20:50:51 +0100

    minor changes in main.js and generateHashes.js

--- backend/generateHashes.js
+++ backend/generateHashes.js
@@ -1,4 +1,10 @@
+require("dotenv").config();
 const bcrypt = require("bcrypt");
+const BCRYPT_SALT_ROUNDS = Number(process.env.BCRYPT_SALT_ROUNDS);
+
+if (Number.isNaN(BCRYPT_SALT_ROUNDS)) {
+  throw new Error("BCRYPT_SALT_ROUNDS no es un número válido");
+}
 
 const passwords = {
   admin: "Admin123!",
@@ -8,7 +14,7 @@ const passwords = {
 
 (async () => {
   for (const [role, pass] of Object.entries(passwords)) {
-    const hash = await bcrypt.hash(pass, 10);
+    const hash = await bcrypt.hash(pass, BCRYPT_SALT_ROUNDS);
     console.log(`${role}: ${hash}`);
   }
 })();
